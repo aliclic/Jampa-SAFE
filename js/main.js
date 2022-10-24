945c188668ac09119be8361e8e44d65220a314d2
João Marcos Chaves <102380570+Joaomarcos7@users.noreply.github.com>
2022-10-24 07:32:54 -0300

    Manipulação LocalStorage

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
  
- const localstoragecards= JSON.parse(localStorage.getItem('cards'))
+ //const localstoragecards= JSON.parse(localStorage.getItem('cards')
 
 
 
 
- const cards= localStorage.getItem('cards')!== null ? localstoragecards : []
+ //const cards= localStorage.getItem('cards')!== null ? localstoragecards : []
  
  
  
- const updatelocalstorage=()=>{
-     localStorage.setItem('cards',JSON.stringify(cards))
- }
-
-
+ //const updatelocalstorage=()=>{
+   //
+/*
+   let cardsJson=[
+    {'id':'0','descrição' : '','bairro' : '', 'data': '','img':''},
+    
+    ]
 
 
  async function getcep(cep,cards_size){
@@ -22,8 +24,10 @@
     console.log(info) //fins de teste!!!
     let bairro=document.getElementById(`bairro${cards_size}`)
     bairro.innerHTML=info.bairro
+   // return bairro.innerHTML (isso aq retorna a Promise, e não de fato o atributo que a gnt quer!)
     }
     
+
 
   
 function addelement(){
@@ -35,9 +39,8 @@ function addelement(){
     card.id=cards_size
     card.classList.add("card");
     card.classList.add("m-5");
-    card.innerHTML=` <img src="" class="card-img-top card-image" id="image${cards_size}" alt="image"> <ul class="list-group list-group-flush"> <li class="list-group-item fw-bold">Tipo:<span class="px-2" id="type${cards_size}"></span></li>  <li class="list-group-item fw-bold">Bairro:<span class="px-2 card-descricao" id="bairro${cards_size}"></span></li>  <li class="list-group-item fw-bold">Data: <span class="px-2 card-descricao" type="date" id='date${cards_size}'></span></li>  <li class="list-group-item fw-bold">Descrição: <span class="px-2 card-descricao" id="description${cards_size}" > </span></li>   </ul>  <div class="card-body d-flex justify-content-around"> <button type="button" class="btn btn-danger card-button" ; onclick="removecard(document.getElementById('${cards_size}') )" >Excluir</button> <button type="button" class="btn btn-warning card-button" onclick='alterarcard(document.getElementById('${cards_size}'))'>Alterar</button> </div> `
+    card.innerHTML=` <img src="" class="card-img-top card-image" id="image${cards_size}" alt="image"> <ul class="list-group list-group-flush"> <li class="list-group-item fw-bold">Tipo:<span class="px-2" id="type${cards_size}"></span></li>  <li class="list-group-item fw-bold">Bairro:<span class="px-2 card-descricao" id="bairro${cards_size}"></span></li>  <li class="list-group-item fw-bold">Data: <span class="px-2 card-descricao" type="date" id='date${cards_size}'></span></li>  <li class="list-group-item fw-bold">Descrição: <span class="px-2 card-descricao" id="description${cards_size}" > </span></li>   </ul>  <div class="card-body d-flex justify-content-around"> <button type="button" class="btn btn-danger card-button" ; onclick="removecard(document.getElementById('${cards_size}') )" >Excluir</button> <button type="button" class="btn btn-warning card-button" onclick='edit(document.getElementById('${cards_size}'))'>Alterar</button> </div> `
     card.style.width='18rem'
-    updatelocalstorage()
     cards.appendChild(card);
     let image=document.getElementById(`image${cards_size}`)
     let file=document.getElementById('formFile')
@@ -58,14 +61,40 @@ function addelement(){
 }
 
 
+
+
 function removecard(card){
 let cards= document.getElementById('cards')
 alert('VOCE ESTA REMOVENDO UM CARD')
 cards.removeChild(card)
 }
 
-function alterarcard(card){
 
+
+function alterarcard(){
+   /* let image=document.getElementById(`image${id}`)
+        let file=document.getElementById('formFile')
+        let tipo=document.getElementById('tipo')
+        let type=document.getElementById(`type${id}`)
+        let description=document.getElementById(`description${id}`)
+        let descrição=document.getElementById('exampleFormControlTextarea1')
+        let date=document.getElementById(`date${id}`)
+        let cep=document.getElementById('CEP')
+        let data=document.getElementById('title')
+    
+    
+        // image.src=file.value
+        type.innerHTML=tipo.value
+        description.innerHTML=descrição.value
+        getcep(cep.value,cards_size)
+        date.innerHTML=data.value.split("-").reverse().join("/") //reversão da string de Data e formatação 
+    }
+
+
+function edit(){
+    /*let button=document.getElementById('modal-button')
+    let card= document.getElementById(id)
+    button.onclick=`alterarcard(${id})` 
 
 }
 
@@ -86,3 +115,6 @@ function validation(){
         cep.style.background=''
         }
  }
+
+ */
+ 
--- js/object.js
+++ js/object.js
@@ -1,27 +1,146 @@
- /* export async function getcep(cep,bairro){
-const url= 'https://viacep.com.br/ws/'+cep+'/json/'
-const response= await fetch(url)
-const info= await response.json()
-console.log(info)
-let bairro=document.getElementById('bairro')
-bairro.innerHTML=info.bairro
-} */
 
+const getlocalstorage=()=> JSON.parse(localStorage.getItem('db')) ?? []//passar para json.parse() se nao  ele ficará sempre string e nao poderá executar metodos como o push, o getitem sempre vai buscar um astring do localstorage
 
-const localstoragecards= JSON.parse(localStorage.getItem('cards'))
+const setlocalstorage=(db_card)=> localStorage.setItem('db',JSON.stringify(db_card))
 
 
 
+//CREATE
+const createcard=(card)=>{
+   let db_card= getlocalstorage()  
+   db_card.push(card)
+   setlocalstorage(db_card)
+}
 
-const cards= localStorage.getItem('cards')!== null ? localstoragecards : []
+//READ
+const readcard=()=> getlocalstorage()
 
+//UPDATE
 
+const updatecard=(card,index) => {
+    const db_card=readcard()
+db_card[index]=card //dados atualizados
+setlocalstorage(db_card)
+}
 
-const updatelocalstorage=()=>{
-    localStorage.setItem('cards',JSON.stringify(cards))
+//ERASE
+
+const deletecard = (index) => {
+    const db_card=readcard()
+    db_card.splice(index,1) //método splice para arrays. param1: índice de start , param2: qtd de elementos a remover!
+}
+const isvalid=()=> {
+   return document.getElementById('form').reportValidity()// método retorna true se todos os required foram atendidos e falso se não!!
 }
 
 
 
+const clearfields=()=>{
+   const fields= document.querySelectorAll('.modal-field') //antes de atualizar os cards da maneira que esta no localstorage, ele exclui o lixo restante no DOM para nao duplicar cards.
+   fields.forEach(
+ field => field.value=''   
+   )
+}
 
+
+
+const createrelato=(card,index)=> {
+    const newrelato=document.createElement('div')
+    newrelato.classList.add("card");
+    newrelato.classList.add("m-5");
+    newrelato.innerHTML=` <img src="${card.img}" class="card-img-top card-image" id="image" alt="image"> <ul class="list-group list-group-flush"> <li class="list-group-item fw-bold">Tipo:<span class="px-2" id="type">${card.tipo}</span></li>  <li class="list-group-item fw-bold">Bairro:<span class="px-2 card-descricao" id="bairro">${card.bairro}</span></li>  <li class="list-group-item fw-bold">Data: <span class="px-2 card-descricao" type="date" id='date'>${card.data}</span></li>  <li class="list-group-item fw-bold">Descrição: <span class="px-2 card-descricao" id="description" >${card.descrição} </span></li>   </ul>  <div class="card-body d-flex justify-content-around"> <button type="button" class="btn btn-danger card-button"; data-action='excluir' id='excluir-${index}'>Excluir</button> <button type="button" class="btn btn-warning card-button" data-bs-toggle="modal" data-bs-target="#exampleModal" data-action='alterar' id='alterar-${index}'>Alterar</button> </div> `
+    newrelato.style.width='18rem'
+
+    const cards=document.getElementById('cards')
+    cards.appendChild(newrelato)
+    }
+
+    const clearcards=()=> {
+        const cards=document.querySelectorAll('.cards .card')
+         //retorna uma array dos elementos correspondente
+        cards.forEach(card => card.parentNode.removeChild(card)) //apagando o proprio elemento filho para evitar repetição dos cards!
+    }
+
+const updatecards=()=>{ 
+    const db_card=readcard()
+    clearcards() // para evitar que ao atualizar os cards se dupliquem na busca no localstorage
+    db_card.forEach(createrelato)
+}
+
+const addcard=()=>{
+
+    if(isvalid()){
+        const card={
+            descrição: document.getElementById('exampleFormControlTextarea1').value,
+            tipo: document.getElementById('tipo').value,
+            bairro: document.getElementById('CEP').value, 
+            data:  document.getElementById('title').value.split('-').reverse().join('/'),
+            img:document.getElementById('formFile').value
+        }
+
+        const index=document.getElementById('CEP').dataset.index
+        if(index=='new'){
+        createcard(card)
+        console.log('cadastrando...')
+        clearfields()
+        updatecards()
+        }
+        else{
+            updatecard(parseInt(index),card)
+            updatecards()
+        }
+    }
+
+
+    else{
+        alert('Voce nao preencheu tudo!')
+    }
+
+
+}
+const preencher=(card)=>{
+            document.getElementById('exampleFormControlTextarea1').value=card.descrição
+            document.getElementById('tipo').value=card.tipo
+            document.getElementById('CEP').value =card.bairro
+            document.getElementById('title').value=card.data
+           //document.getElementById('formFile').value=card.img
+           document.getElementById('CEP').dataset.index=card.index
+}
+
+
+
+const editarcard=(index)=>{
+    const card=readcard()[index]
+    card.index=index
+     preencher(card)
+
+}
+
+
+
+const editdelet=(event)=> {
+    console.log(event.target.type) // retorna um pointerveent, target te retorna qual elemento vc clicou! type vai te informar o type do botao ja que so apenas o botoes possuem esse atributo
+
+    if (event.target.type==='button'){
+       const [action,index] = event.target.id.split('-') //maneira diferente de nomear os indices de uma array
+       if (action=='alterar'){
+        editarcard(parseInt(index))
+       }
+       else{
+        const response= confirm('Deseja mesmo excluir o seu Relato?')
+        if(response){
+       deletecard(parseInt(index))
+       updatecards()
+        }
+       }
+    }
+}
+
+
+
+document.getElementById('modal-button').addEventListener('click',addcard) //param1: tipo do evento; param2: o que vai ser realizado, função sem o () senão vai  dar erro!!!!
+
+
+
+document.querySelector('.cards').addEventListener('click',editdelet)
 
